ef64edeeaad7deb62c86d1207f6d2ee3aae3b366
gitolite <gitolite@78.47.177.148:11122/cms5>
2015-06-24 12:15:25 +0300

    added model

---
+++ app/models/message.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  avatar: DS.attr('string'),
+  username: DS.attr('string'),
+  message: DS.attr('string')
+});
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('message-list');
+  this.route('message-list', {path: 'chat'});
 });
 
 export default Router;
