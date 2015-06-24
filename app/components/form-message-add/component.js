501cd8aa0497e1ac3bb138ac4740391763d23ce9
gitolite <gitolite@78.47.177.148:11122/cms5>
2015-06-24 12:33:37 +0300

    added send message added message list component

--- app/components/form-message-add/component.js
+++ app/components/form-message-add/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	actions: {
+        addMessage(message) {
+            var avatar = 'avatar';
+            var username = 'username';
+			
+            this.sendAction('addMessage', {avatar, username, message});
+        }
+    }
 });
--- app/message-list/route.js
+++ app/message-list/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return this.store.find('message');
+	},
+	
+	actions: {
+        sendMessage(message) {
+            const messageModel = this.store.createRecord('message', message);
+            messageModel.save();
+        }
+    }
 });
