f1f374ac45aa3534672c39bbb92425a1f26c291a
vladislav.goshkovich <vgoshkovych@geeksforless.net>
2015-07-15 22:35:55 +0300

    added twitter oauth

---
+++ app/initializers/firebase-session.js
@@ -0,0 +1,30 @@
+
+import Base from 'simple-auth/authenticators/base';
+import Firebase from 'firebase';
+import config from '../config/environment';
+
+const firebase = new Firebase(config.firebase)
+
+const FirebaseAuthenticator = Base.extend({
+  authenticate (provider) {
+    return new Promise((resolve, reject) => {
+      firebase.authWithOAuthPopup(provider, (error, authData) => {
+        error ? reject(error) : resolve(authData);
+      });
+    });
+  },
+  restore (data) {
+    return new Promise((resolve, reject) => {
+      data.provider && data[data.provider] ? resolve(data) : reject();
+    });
+  }
+});
+
+export function initialize( container, application ) {
+  application.register('authenticator:firebase', FirebaseAuthenticator);
+}
+
+export default {
+  name: 'firebase-session',
+  initialize: initialize
+};
